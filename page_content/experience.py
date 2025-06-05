4c7b5bab5801b5f8c77917f5f9ee0f2c332f15ac
pyl0229 <pyl100229@gmail.com>
2025-06-05 16:53:41 +0800

    Update experience.py

--- page_content/experience.py
+++ page_content/experience.py
@@ -8,41 +8,18 @@ def experience_page():
     ### Corporate Communications and Public Affairs Intern
     **Burson Cohn & Wolfe** | *April 2024 - July 2024*
     
-    - Information collection and analysis: Conducted comprehensive industry research for the Lenovo brand across 
-platforms such as TechCrunch and Engadget. Performed comparative data analysis to identify and articulate 
-Lenovo's distinct competitive advantages in technology R&D, product innovation, and customer service. 
-Developed strategic advantage analysis reports to enhance brand visibility and content reach; 
-    - Risk early warning response: Utilized the risk matrix model to quantitatively evaluate potential risks impacting 
-the Lenovo brand, considering both domestic and international public sentiment and policy shifts. Generated 
-a conclusion with key areas of concern and their potential impact on the company's operations and 
-collaborated with legal affairs team, to develop and implement mitigation strategies for identified risks. 
-Successfully warned 2 intermediate potential risks in advance to support the maintenance of the company's 
-business. 
-    - Content output promotion: Participated in the daily operation and management of Lenovo corporate public 
-account. Responsible for the compilation of specific content. Fostered relationships with international media 
-outlets to enhance Lenovo's external publicity efforts. Developed and executed content strategies aligned 
-with key publicity objectives, resulting in a 30% increase in overall exposure for the brand.
+    - Information collection and analysis: Conducted comprehensive industry research for the Lenovo brand across platforms such as TechCrunch and Engadget. Performed comparative data analysis to identify and articulate Lenovo's distinct competitive advantages in technology R&D, product innovation, and customer service. Developed strategic advantage analysis reports to enhance brand visibility and content reach; 
+    - Risk early warning response: Utilized the risk matrix model to quantitatively evaluate potential risks impacting the Lenovo brand, considering both domestic and international public sentiment and policy shifts. Generated a conclusion with key areas of concern and their potential impact on the company's operations and collaborated with legal affairs team, to develop and implement mitigation strategies for identified risks. Successfully warned 2 intermediate potential risks in advance to support the maintenance of the company's business. 
+    - Content output promotion: Participated in the daily operation and management of Lenovo corporate public account. Responsible for the compilation of specific content. Fostered relationships with international media outlets to enhance Lenovo's external publicity efforts. Developed and executed content strategies aligned with key publicity objectives, resulting in a 30% increase in overall exposure for the brand.
     """)
     
     st.markdown("""
     ### Commodity Operation Intern
     **Banana In** | *August 2022 - November 2022*
     
-    - Commodity Selection analysis: Conducted a thorough analysis of over 600 SKUs, examining sales, purchase rate, 
-and profit rate across multiple timeframes, including sequential, year-on-year, last month, last quarter, the 
-same period of last year, and the past year to complete a priority ranking for SKUs. Anchored new product 
-selections in alignment with current industry trends and performed a comparative analysis of competitive 
-products' bestsellers against our own offerings to identify unique selling points and areas for 
-differentiation.Leveraged our competitive advantages to enhance product differentiation in the market. 
-    - Explosive product marketing: Conducted in-depth analysis to identify and target key demographics (e.g.healthconscious
- parents in second-tier cities), for selected explosive products. Analyzed demand pain points including 
-product use scenarios and functions to determine primary selling points (e.g. multi-functional versions and 
-pattern types). Developed an integrated online and offline marketing strategy focusing on key scenarios (e.g. 
-family travel and playground activities), which included social media promotion and offline event planning, 
-effectively increasing exposure.
-    - Strategy support: Supported strategic initiatives during peak sales seasons, monitoring market trends and 
-consumer insights to refine product, pricing, and promotion strategies. Achieved a 20% sales boost and a 15% 
-increase in inventory turnover for key products.
+    - Commodity Selection analysis: Conducted a thorough analysis of over 600 SKUs, examining sales, purchase rate, and profit rate across multiple timeframes, including sequential, year-on-year, last month, last quarter, the same period of last year, and the past year to complete a priority ranking for SKUs. Anchored new product selections in alignment with current industry trends and performed a comparative analysis of competitive products' bestsellers against our own offerings to identify unique selling points and areas for differentiation.Leveraged our competitive advantages to enhance product differentiation in the market. 
+    - Explosive product marketing: Conducted in-depth analysis to identify and target key demographics (e.g.healthconscious parents in second-tier cities), for selected explosive products. Analyzed demand pain points including product use scenarios and functions to determine primary selling points (e.g. multi-functional versions and pattern types). Developed an integrated online and offline marketing strategy focusing on key scenarios (e.g. family travel and playground activities), which included social media promotion and offline event planning, effectively increasing exposure.
+    - Strategy support: Supported strategic initiatives during peak sales seasons, monitoring market trends and consumer insights to refine product, pricing, and promotion strategies. Achieved a 20% sales boost and a 15% increase in inventory turnover for key products.
     """)
     
     st.markdown("---")
@@ -52,11 +29,7 @@ increase in inventory turnover for key products.
     projects = [
         {
             "title": "Xiaohongshu Hong Kong User Acquisition Project",
-            "description": "The project aimed to enhance Xiaohongshu's recognition and market share in Hong Kong 
-by conducting user research to gather key insights for market strategies. Content analysis utilized big data to 
-understand user behavior patterns and optimize content direction. Creative marketing activities, combined with 
-merchant collaborations and offline advertising, successfully encouraged users to download Xiaohongshu, with 
-an expected online exposure of over 500,000. ",
+            "description": "The project aimed to enhance Xiaohongshu's recognition and market share in Hong Kong by conducting user research to gather key insights for market strategies. Content analysis utilized big data to understand user behavior patterns and optimize content direction. Creative marketing activities, combined with merchant collaborations and offline advertising, successfully encouraged users to download Xiaohongshu, with an expected online exposure of over 500,000. ",
             "skills": ["Python", "scikit-learn", "Pandas"],
             "link": "https://mycuhk-my.sharepoint.com/:f:/g/personal/1155220586_link_cuhk_edu_hk/EqMKTfMjKzdAiu3E7Fc81PYBBefk1zRmHM-GKO16flQoQg?e=BisdYT"
         },
