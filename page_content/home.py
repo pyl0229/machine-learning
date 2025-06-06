6986f85aea6de37c50d76ee4d849bee1c034fbb9
pyl0229 <pyl100229@gmail.com>
2025-06-06 14:58:08 +0800

    Update home.py

--- page_content/home.py
+++ page_content/home.py
@@ -6,11 +6,11 @@ def home_page():
     left_col, right_col = st.columns(2)
     left_col.markdown(
         """
-        <h4>Sarah Johnson</h4>
+        <h4>Yilin Peng</h4>
         <p>Recent Master's Graduate in Marketing<br>
         Chinese University of Hong Kong<br>
         12 Chak Cheung St., Ma Liu Shui, HKSAR<br>
-        <a href="mailto:sarah.johnson@example.com">sarah.johnson@example.com</a></p>
+        <a href="mailto:pyl100229@gmail.com">pyl100229@gmail.com</a></p>
         """,
         unsafe_allow_html=True
     )
@@ -28,24 +28,19 @@ def home_page():
     st.markdown(
         """
         ### About Me
-        I am a recent master's graduate in Data Science from the University of XYZ, eager to apply my knowledge and skills in a professional setting. During my academic journey, I developed a strong foundation in statistical analysis, machine learning, and data visualization.
+        Outstanding learning ability: capable of quickly mastering and applying data analysis tools and market research methodologies, effectively transforming the learned knowledge into strategic planning and decision support in practical work;
 
-        As part of my master's program, I completed several projects that involved working with real-world datasets and applying various data science techniques. These projects allowed me to gain hands-on experience in data preprocessing, exploratory data analysis, model building, and evaluation.
+        Outstanding planning ability: Through in-depth analysis of sales data of over 600 SKUs across multiple time dimensions, scientifically planned product selection priorities and integrated online and offline marketing plans.
 
-        I am passionate about leveraging data to drive insights and make informed decisions. I am a quick learner, a collaborative team player, and possess strong problem-solving skills. I am excited to contribute my skills and grow as a data science professional in a dynamic and challenging environment.
+        Outstanding content capabilities: capable of systematically sorting out and analyzing complex information, and constructing a clear logical framework for market and brand development.
         """
     )
 
     st.markdown(
         """
         ### Skills
-        - Programming Languages: Python, R
-        - Data Analysis: Pandas, NumPy, Matplotlib, Seaborn
-        - Machine Learning: Scikit-learn, TensorFlow, Keras
-        - Database: SQL, MongoDB
-        - Data Visualization: Tableau, Power BI
-        - Statistical Analysis: Hypothesis Testing, Regression Analysis
-        - Communication: Presentation Skills, Technical Writing
+        - Programming Languages: Python, R, SQL
+        - Communication: Mandarin (Native), Cantonese (Native), English (Fluent), French (Basic)
         """
     )
 
